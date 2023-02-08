281a96cb05a33da9ff784482e67c74a49b0733e9
torgerbocianowski <bocianot@hu-berlin.de>
2023-02-08 15:00:48 +0100

    adding existing files to repo

---
+++ pages/api/stream.ts
@@ -0,0 +1,68 @@
+import {
+  createParser,
+  ParseEvent,
+  ReconnectInterval,
+} from "eventsource-parser";
+
+export interface StreamPayload {
+  model: string;
+  prompt: string;
+  temperature: number;
+  top_p: number;
+  frequency_penalty: number;
+  presence_penalty: number;
+  max_tokens: number;
+  stream: boolean;
+  n: number;
+}
+
+export async function Stream(payload: StreamPayload) {
+  const encoder = new TextEncoder();
+  const decoder = new TextDecoder();
+
+  let counter = 0;
+
+  const res = await fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    },
+    body: JSON.stringify(payload),
+  });
+
+  const stream = new ReadableStream({
+    async start(controller) {
+      function onParse(event: ParseEvent | ReconnectInterval) {
+        if (event.type === "event") {
+          const data = event.data;
+
+          if (data === "[DONE]") {
+            controller.close();
+            return;
+          }
+
+          try {
+            const json = JSON.parse(data);
+            const text = json.choices[0].text;
+            if (counter > 2 && (text.match(/\n/) || []).length) return;
+
+            const queue = encoder.encode(text);
+            controller.enqueue(queue);
+            counter++;
+          } catch (error) {
+            controller.error(error);
+          }
+        }
+      }
+
+      const parser = createParser(onParse);
+      // https://web.dev/streams/#asynchronous-iteration
+      for await (const chunk of res.body as any) {
+        parser.feed(decoder.decode(chunk));
+      }
+    },
+  });
+
+  return stream;
+}
